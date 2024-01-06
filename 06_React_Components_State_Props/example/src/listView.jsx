8336da1a4b44a7b6892b297dcec8867387b9c074
amitdhamsaniya <dhamsaniyaamit@gmail.com>
2024-01-06 13:45:30 +0530

    data

---
+++ 06_React_Components_State_Props/example/src/listView.jsx
@@ -0,0 +1,50 @@
+import React ,  { useState } from "react";
+import Button from "react-bootstrap/Button";
+
+export default function ListView() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  return (
+    <>
+      <div
+        className="container d-flex justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="row align-items-center">
+          <div className="col  ">
+            <h1 className="text-center ">Increment/Decrement Example</h1>
+            <h2 className="text-center my-5 fs-1">{count}</h2>
+            <div className="text-center">
+              <Button
+                variant="danger"
+                className="mx-3 px-3 border"
+                onClick={decrement}
+              >
+                Decrement
+              </Button>
+              <Button
+                variant="success"
+                className="mx-3 px-3 border"
+                onClick={increment}
+              >
+                Increment
+              </Button>
+            </div>
+            <div className="text-center">
+              <Button
+                variant="secondary"
+                className="my-5 px-3 border"
+                onClick={reset}
+                disabled={count === 0}
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
